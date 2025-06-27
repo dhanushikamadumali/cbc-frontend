2e20aafc8eb7ba321346447e182e7a36de957b80
dhanushika <dhanushika76@gmail.com>
2025-06-27 11:24:29 +0530

    otp password

---
+++ src/pages/client/forgetPassword.jsx
@@ -0,0 +1,17 @@
+export default function forgetPassword(){
+    return(
+        <div className="w-full h-screen bg-gray-200 flex p-2">
+            <div className="w-full h-full flex items-center justify-center">
+                <div className="bg-white p-4 rounded shadow-md w-[400px]">
+                    <h1 className="text-2xl font-bold mb-4">Forget Password</h1>
+                    <form>
+                        <div className="mb-4">
+                            <label htmlFor="email">Email</label>
+                        </div>
+                    </form>
+                    </div>
+            
+            </div>
+        </div>
+    )
+}
